41897d2c5ad92639399fc3bf88db543c64f7864a
Tl2851 <99358309+Tl2851@users.noreply.github.com>
2022-02-11 11:31:50 -0800

    Update Card.java

--- src/Card.java
+++ src/Card.java
@@ -1,5 +1,47 @@
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+
+import javax.tools.DocumentationTool.Location;
 
 /** represents a playing card that can draw itself. */
-public class Card implements Drawable, Updateable{
+public class Card implements Drawable, Updateable{    
 
+private int suite;
+private int value;
+private boolean faceup;
+private Location x,y;
+
+
+public Card(int suite, int value){
+    this.suite=suite;
+    this.value=value;
+}
+
+
+public void setSuite(int suite) {
+    this.suite = suite;
+}
+public void setValue(int value) {
+    this.value = value;
+}
+
+public int getSuite() {
+    return suite;
+}
+public int getValue() {
+    return value;
+}
+
+
+@Override
+public void update(ActionEvent a) {
+    // TODO Auto-generated method stub
+    
+}
+
+@Override
+public void draw(Graphics g) {
+    // TODO Auto-generated method stub
+    
+}
 }
