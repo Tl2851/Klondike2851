75c8a24b34e8f813976a98a00e824b583f1f1a78
Tl2851 <99358309+Tl2851@users.noreply.github.com>
2022-02-28 11:05:55 -0800

    Update Pile.java

--- src/Pile.java
+++ src/Pile.java
@@ -1,14 +1,5 @@
+import java.util.*;
 
-import java.awt.event.ActionEvent;
-
-import javax.tools.DocumentationTool.Location;
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 /** A Pile is a collection of cards.  This needs to be
  * Drawable because it will be shown on the GUI. Put code
  * here that ALL Piles share.  The ways in which Piles are 
@@ -17,25 +8,14 @@ import java.util.Arrays;
  * You WILL write subclasses of Pile
  */
 public abstract class Pile implements Drawable, Updateable {
+    
+    public abstract boolean canAddCard(Card c);
 
-  public abstract boolean canAddCard(Card c);
+    // private ArrayList<Card> cards;
+    // private int x, y;
 
-	
-	private ArrayList <Card> cardPile = new ArrayList <>();
-
-	private int pilex, piley;
-
-	
-	public Card draw(){
-		return cardPile.remove(cardPile.size()-1);
-	}
-
-
-	public void add(Card c){
-		cardPile.add(c);
-	}
-
-	
-	
-	
+    // public Pile(int x, int y) {
+    //     this.x = x;
+    //     this.y = y;
+    // }
 }
